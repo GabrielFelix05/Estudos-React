577887a17db9bad3f529425875f90c8c59d865ee
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-09-07 11:37:33 -0300

    Api useState

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
+  const [tarefas, setTarefas] = useState([
+    "Pagar a conta de luz",
+    "Estudar React"
+  ])
+
+  const[input, setInput] = useState()
+
+  function handleAdd(){
+    setTarefas([...tarefas, input])
+  }
+
   return (   
     <div>
-      <p>Olá</p>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+      <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   )
 }
