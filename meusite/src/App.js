367e9fd5a58fd374b8900cf246fcd352cbc76296
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2022-05-31 07:55:04 -0300

    aula hooks

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import Routes from './routes';
+import React, {useState} from 'react'
 
 function App() {
+  const [tarefas, setTarefas] = useState([
+    'paragas a conta',
+    'estudar react hooks'
+  ])
+
+  const [input, setInput] = useState()
+
+  function handleAdd() {
+    setTarefas([...tarefas, input])
+  }
+
   return (   
     <div>
-      <Routes/>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+      <button type='button' onClick={handleAdd}>Adicionar</button>
     </div>
   )
 }
