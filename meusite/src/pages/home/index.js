9d07eb906c81c69f4f9351db518f05670fa4c39a
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2022-06-03 08:47:48 -0300

    Carregando filmes

--- meusite/src/pages/home/index.js
+++ meusite/src/pages/home/index.js
@@ -6,6 +6,7 @@ import './styles.css'
 
 function Home() {
     const [filmes, setFilmes] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         
@@ -19,10 +20,20 @@ function Home() {
             })
             
             setFilmes(response.data.results.slice(0, 10))
+            setLoading(false)
         }
 
         loadFilmes()
     }, [])
+
+    if(loading) {
+        return (
+            <div className='loading'>
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
+
     return (
         <div className='container'>
             <div className='lista-filmes'>
--- meusite/src/routes.js
+++ meusite/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route} from 'react-router-dom'
 import Header from './components/header'
 import Home from './pages/home'
 import Filme from './pages/filme'
+import Error from './pages/error'
 
 
 function RoutesApp() {
@@ -12,6 +13,7 @@ function RoutesApp() {
             <Switch>
                 <Route path="/" component={Home} exact/>
                 <Route path="/filme/:id" component={Filme} exact/>
+                <Route path="*" component={Error} />
             </Switch>
         </BrowserRouter>
     )
